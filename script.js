182ec75e8e8ea75713a349a5356a949c3da88a5a
Kalai <kalaiselvi@expsoltechs.com>
2020-03-24 20:42:31 +0530

    adminpanel

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function closeSidebar() {
  // Charts
  let myChart = document.getElementById('myChart').getContext('2d');
  let myReport = document.getElementById('myReport').getContext('2d');
-
+ let myBarChart = document.getElementById('bar_chart').getContext('2d');
 
  // Global Options
  Chart.defaults.global.defaultFontFamily = 'Lato';
@@ -150,6 +150,72 @@ function closeSidebar() {
   }
 });
 
+
+let massBarChart = new Chart(myBarChart, {
+  type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+  data:{
+    labels:['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet', 'Lorem'],
+    datasets:[{
+      label:'Data',
+      data:[
+        617594,
+        181045,
+        153060,
+        566519,
+        105162,
+        95072
+      ],
+      //backgroundColor:'green',
+      backgroundColor:[
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(54, 162, 235, 0.6)',
+        'rgba(255, 206, 86, 0.6)',
+        'rgba(75, 192, 192, 0.6)',
+        'rgba(153, 102, 255, 0.6)',
+        'rgba(255, 159, 64, 0.6)',
+        'rgba(255, 99, 132, 0.6)'
+      ],
+     
+    }]
+  },
+  options:{
+    title:{
+      display:true,
+      //text:'Largest Cities In Massachusetts',
+      fontSize:25
+    },
+    scales: {
+      xAxes: [{
+          gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }
+      }],
+      yAxes: [{
+          gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }   
+      }]
+  },
+    legend:{
+      display:false,
+      position:'right',
+      labels:{
+        fontColor:'#000'
+      }
+    },
+    layout:{
+      padding:{
+        left:0,
+        right:0,
+        bottom:0,
+        top:0
+      }
+    },
+    tooltips:{
+      enabled:true
+    }
+  }
+});
 
 
 
