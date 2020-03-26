3f946d32880f517ff7e21f7badbe7f253e1ab528
Kalai <kalaiselvi@expsoltechs.com>
2020-03-26 14:47:20 +0530

    Template

--- script.js
+++ script.js
@@ -21,202 +21,186 @@ function closeSidebar() {
 
 
 
- // Charts
- let myChart = document.getElementById('myChart').getContext('2d');
- let myReport = document.getElementById('myReport').getContext('2d');
- let myBarChart = document.getElementById('bar_chart').getContext('2d');
 
- // Global Options
- Chart.defaults.global.defaultFontFamily = 'Lato';
- Chart.defaults.global.defaultFontSize = 14;
- Chart.defaults.global.defaultFontColor = '#222';
+const counters = document.querySelectorAll('.counter');
+const speed = 100; // The lower the slower
 
- let massPopChart = new Chart(myChart, {
-   type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-   data:{
-     labels:['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet', 'Lorem'],
-     datasets:[{
-       label:'Data',
-       data:[
-         617594,
-         181045,
-         153060,
-         106519,
-         105162,
-         95072
-       ],
-       //backgroundColor:'green',
-       backgroundColor:[
-         'rgba(255, 99, 132, 0.6)',
-         'rgba(54, 162, 235, 0.6)',
-         'rgba(255, 206, 86, 0.6)',
-         'rgba(75, 192, 192, 0.6)',
-         'rgba(153, 102, 255, 0.6)',
-         'rgba(255, 159, 64, 0.6)',
-         'rgba(255, 99, 132, 0.6)'
-       ],
-      
-     }]
-   },
-   options:{
-     title:{
-       display:true,
-       //text:'Largest Cities In Massachusetts',
-       fontSize:25
-     },
-     scales: {
-      xAxes: [{
-          gridLines: {
-              color: "rgba(0, 0, 0, 0)",
-          }
-      }],
-      yAxes: [{
-          gridLines: {
-              color: "rgba(0, 0, 0, 0)",
-          }   
-      }]
-  },
-     legend:{
-       display:true,
-       position:'right',
-       labels:{
-         fontColor:'#000'
-       }
-     },
-     layout:{
-       padding:{
-         left:30,
-         right:0,
-         bottom:0,
-         top:0
-       }
-     },
-     tooltips:{
-       enabled:true
-     }
-   }
- });
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
 
- let massRepChart = new Chart(myReport, {
-  type:'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-  data:{
-    labels:['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet', 'Lorem'],
-    datasets:[{
-      label:'Data',
-      data:[
-        617594,
-        181045,
-        153060,
-       
-      ],
-      //backgroundColor:'green',
-      backgroundColor:[
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
-        'rgba(255, 206, 86, 0.6)',
-        'rgba(75, 192, 192, 0.6)',
-        'rgba(153, 102, 255, 0.6)',
-        'rgba(255, 159, 64, 0.6)',
-        'rgba(255, 99, 132, 0.6)'
-      ],
-     
-    }]
-  },
-  options:{
-    title:{
-      display:true,
-      //text:'Largest Cities In Massachusetts',
-      fontSize:25
-    },
- 
-    legend:{
-      display:false,
-      position:'right',
-      labels:{
-        fontColor:'#000'
-      }
-    },
-    layout:{
-      padding:{
-        left:0,
-        right:0,
-        bottom:0,
-        top:0
-      }
-    },
-    tooltips:{
-      enabled:true
-    }
-  }
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
 });
 
 
-let massBarChart = new Chart(myBarChart, {
-  type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-  data:{
-    labels:['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet', 'Lorem'],
-    datasets:[{
-      label:'Data',
-      data:[
-        617594,
-        181045,
-        153060,
-        566519,
-        105162,
-        95072
-      ],
-      //backgroundColor:'green',
-      backgroundColor:[
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
-        'rgba(255, 206, 86, 0.6)',
-        'rgba(75, 192, 192, 0.6)',
-        'rgba(153, 102, 255, 0.6)',
-        'rgba(255, 159, 64, 0.6)',
-        'rgba(255, 99, 132, 0.6)'
-      ],
-     
-    }]
-  },
-  options:{
-    title:{
-      display:true,
-      //text:'Largest Cities In Massachusetts',
-      fontSize:25
-    },
-    scales: {
-      xAxes: [{
-          gridLines: {
-              color: "rgba(0, 0, 0, 0)",
+let myChart = document.getElementById('myChart').getContext('2d');
+
+let bar_chart = document.getElementById('bar_chart').getContext('2d');
+    // Global Options
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 14;
+    Chart.defaults.global.defaultFontColor = '#222';
+
+    let massPopChart = new Chart(myChart, {
+      type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+      data:{
+        labels:['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[{
+          label:'Population',
+          data:[
+            617594,
+            181045,
+            153060,
+            106519,
+            105162,
+            95072
+          ],
+          fill: false,
+          backgroundColor:'#2196f3',
+          backgroundColor:[
+              '#b7acde'
+          ],
+          borderWidth:1,
+          borderColor:'#2196f3',
+          hoverBorderWidth:3,
+          hoverBorderColor:'#000'
+        }]
+      },
+      options:{
+        maintainAspectRatio: false, 
+        title:{
+          display:true,
+         // text:'Largest Cities In Massachusetts',
+          fontSize:25
+        },
+        scales: {
+          xAxes: [{
+              gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+              }
+          }],
+          yAxes: [{
+              gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+              }   
+          }]
+      },
+        legend:{
+          display:false,
+          position:'right',
+          labels:{
+            fontColor:'#000'
           }
-      }],
-      yAxes: [{
-          gridLines: {
-              color: "rgba(0, 0, 0, 0)",
-          }   
-      }]
-  },
-    legend:{
-      display:false,
-      position:'right',
-      labels:{
-        fontColor:'#000'
+        },
+        layout:{
+          padding:{
+            left:00,
+            right:0,
+            bottom:0,
+            top:0
+          }
+        },
+        tooltips:{
+          enabled:true
+        }
       }
-    },
-    layout:{
-      padding:{
-        left:0,
-        right:0,
-        bottom:0,
-        top:0
+    });
+    
+    let massBarChart = new Chart(bar_chart, {
+      type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+      data:{
+        labels:['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[{
+          label:'Population',
+          data:[
+            12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1
+          ],
+          //backgroundColor:'green',
+          backgroundColor:[
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderWidth:1,
+          borderColor:    [ 'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        
+        }]
+      },
+      options:{
+        maintainAspectRatio: false, 
+        title:{
+          display:true,
+         // text:'Largest Cities In Massachusetts',
+          fontSize:25
+        },
+        scales: {
+          xAxes: [{
+              gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+              }
+          }],
+          yAxes: [{
+              gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+              }   
+          }]
+      },
+        legend:{
+          display:false,
+          position:'right',
+          labels:{
+            fontColor:'#000'
+          }
+        },
+        layout:{
+          padding:{
+            left:0,
+            right:0,
+            bottom:0,
+            top:0
+          }
+        },
+        tooltips:{
+          enabled:true
+        }
       }
-    },
-    tooltips:{
-      enabled:true
-    }
-  }
-});
-
-
-
- 
+    });
